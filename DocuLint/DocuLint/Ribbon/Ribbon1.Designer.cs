c7fdbbcdc59b5c90e52208ee60243356861015ce
jiayw <jiayw530@163.com>
2026-08-21 10:14:12 +0800

    release: 发布 0.0.1.8 更新测试包

--- DocuLint/DocuLint/Ribbon/Ribbon1.Designer.cs
+++ DocuLint/DocuLint/Ribbon/Ribbon1.Designer.cs
@@ -689,7 +689,7 @@ namespace DocuLint
             // btnHelpVersion
             // 
             this.btnHelpVersion.Enabled = false;
-            this.btnHelpVersion.Label = "版本号：0.0.1.7";
+            this.btnHelpVersion.Label = "版本号：0.0.1.8";
             this.btnHelpVersion.Name = "btnHelpVersion";
             this.btnHelpVersion.OfficeImageId = "Info";
             this.btnHelpVersion.ShowImage = true;
